2548555b4b107aea13828e221f96ef9935e3db7b
Jooshyyyy <josephjoshuacandiadorado@gmail.com>
2025-10-14 17:34:00 -0400

    feat/ControllerProvider

--- SCM_PROJECT/app/Http/Controllers/ProviderController.php
+++ SCM_PROJECT/app/Http/Controllers/ProviderController.php
@@ -7,59 +7,55 @@ use Illuminate\Http\Request;
 
 class ProviderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        $providers= Provider::all();
+        return view('providers.index',compact('providers'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
-        //
+        return view('privders.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name'=> 'required|unique:providers.name',
+        ]);
+
+        Provider::create($request->all());
+        return redirect()->route('providers.index')->with('success','Proveedor creado exitosamente');
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Provider $provider)
     {
-        //
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(Provider $provider)
     {
-        //
+        return view('providers.edit',compact('provider'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Provider $provider)
     {
-        //
+        $request->validate([
+            'name'=>'required|unique:providers,name,'. $provider->id,
+        ]);
+        $provider->update($request->all());
+        return redirect()->route('providers.index')->with('success','Proveedor actualizado correctamente');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(Provider $provider)
     {
-        //
+        $provider->delete();
+        return redirect()->route('providers.index')->with('success','Proveedor eliminado correctamente');
     }
 }
