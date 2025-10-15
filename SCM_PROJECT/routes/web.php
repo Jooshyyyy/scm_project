2ccc635b8aeb6d1ca7a91545781422ce95fb0279
Jooshyyyy <josephjoshuacandiadorado@gmail.com>
2025-10-15 16:18:03 -0400

    feat/ControllerProduct

--- SCM_PROJECT/routes/web.php
+++ SCM_PROJECT/routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\ProviderController;
+use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('roles', RoleController::class);
 Route::resource('providers', ProviderController::class);
+Route::resource('produtcs', ProductController::class);
