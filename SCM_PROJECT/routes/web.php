bce65249328d11a6ec05288c8c6b9a31768ff543
Jooshyyyy <josephjoshuacandiadorado@gmail.com>
2025-10-15 15:29:47 -0400

    fix/web.php no provider controller

--- SCM_PROJECT/routes/web.php
+++ SCM_PROJECT/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ProviderController;
 Route::get('/', function () {
     return view('welcome');
 });
