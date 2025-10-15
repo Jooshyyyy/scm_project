44d7842fcf620bc5cf9a9092851269311d749218
Jooshyyyy <josephjoshuacandiadorado@gmail.com>
2025-10-15 15:27:22 -0400

    feat/ViewsProviders

---
+++ SCM_PROJECT/resources/views/providers/edit.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+    <h1>Editar Proveedor</h1>
+    <form action="{{route(providers.update),$provider->id }} " method="POST">
+        @csrf
+        @method('PUT')
+        <label >Nombre:</label>
+        <input type="text" name="name" required>
+        <label >Contacto:</label>
+        <input type="text" name="contact" required>
+        <label >email:</label>
+        <input type="text" name="email" required>
+        <label >Productos:</label>
+        <input type="text" name="products" required>
+        <button type="submit">Actualizar</button>
+    </form>
+@endsection
